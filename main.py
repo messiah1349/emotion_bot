d5a83aa606f92e2a453c1a4da66a19be23b9f33e
messiah1349 <messiah1349@gmail.com>
2023-05-08 16:43:44 +0300

    comment

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ if __name__=='__main__':
     POSTGRE_PASSWORD = os.getenv('POSTGRE_PASSWORD', '1234')
     POSTGRE_HOST = os.getenv('POSTGRE_HOST', 'localhost')
     POSTGRE_PORT = os.getenv('POSTGRE_PORT', '1349')
-    API_TOKEN = os.getenv('EMOTION_BOT_TOKEN', '214139458:AAH8UGU0PW3vUE1lRz-gjXnlB6TroUvpfUk')
+    API_TOKEN = os.getenv('EMOTION_BOT_TOKEN', '214139458:AAH8UGU0PW3vUE1lRz-gjXnlB6TroUvpfUk') 
+    # just a test bot by default for testing
     
     engine = get_engine(POSTGRE_PASSWORD, POSTGRE_PORT, POSTGRE_HOST)
     create_data_base_and_tables(engine)
